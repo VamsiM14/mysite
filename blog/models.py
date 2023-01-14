9262b8f0289cc0a511d0630de3e8ed1f0e7292e9
VamsiM14 <40723294+VamsiM14@users.noreply.github.com>
2023-01-14 11:10:18 +0530

    Add updates

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                    .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -21,6 +26,8 @@ class Post(models.Model):
         User, 
         on_delete=models.CASCADE,
         related_name='blog_posts')
+    objects = models.Manager() # The Default Manager
+    published = PublishedManager() # Custom Manager
     class Meta():
         ordering = ['-publish']
         indexes = [
