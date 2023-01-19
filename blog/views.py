1bc391d0dc26dfcd4da6498e3511e37b17714335
VamsiM14 <40723294+VamsiM14@users.noreply.github.com>
2023-01-19 13:31:28 +0530

    Add features

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number) 
+    except(EmptyPage):
+        # Handle out of range pages
+        posts = paginator.page(paginator.num_pages)
+    except(PageNotAnInteger):
+        # Handle non-integer page inputs
+        posts = paginator.page(1)  
     return render(
         request,
         'blog/post/list.xhtml',
         {'posts': posts} 
     )
 
+class PostListView(ListView):
+    '''
+    Alternative post list view
+    '''
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.xhtml'
+    
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
-    )
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
 
     return render(
         request,
